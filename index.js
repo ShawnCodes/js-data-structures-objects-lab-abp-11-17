976cb22c16c3d87092ffd690e590567795fcba6e
Shawn T <shawn@huthunt.com>
2018-01-21 14:35:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const driver = {
 };
 
 function updateDriverWithKeyAndValue(obj, key, value){
+  const newObj = { ...obj };
+
+  newObj[key] = value;
+
+  return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
   obj[key] = value;
 
 return obj;
 }
-
